da11d5d9776fecab3cb621235cb037a96e5d0a73
SpiderLi <1031824401@qq.com>
2024-11-13 18:30:13 +0800

    feat(message): test

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,9 @@
+const { defineConfig } = require('cypress')
 module.exports = {
-	projectId: "3my7m9",
-	// ...rest of the Cypress project config
-  }
+  // ...rest of the Cypress project config
+  projectId: "3my7m9",
+
+  e2e: {
+    specPattern: 'cypress/integration/**/*.js',  // 可以改成你实际的文件后缀
+  },
+};
